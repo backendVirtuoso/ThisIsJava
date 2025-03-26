1b11d3fd98bf8a5175de626db86de2a08cf7cf3a
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2025-03-26 22:40:05 +0900

    add file ch 3. 연산자

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("이것이 자바다 개정판 책 스터디 Java 8 ~ Java 17");
+        System.out.println("[책 공부] 이것이 자바다 개정판 Java 8 ~ 17 + Java 21 (3판 한빛 사이트 예제소스 참고)");
     }
 }
